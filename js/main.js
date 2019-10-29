00c355c1840b75a1b1ce9c6cbcf7560a175d2130
Simon Beijer <beijersimon@gmail.com>
2019-10-29 11:34:11 +0100

    First name enter div

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let playerName
+
+/**
+*
+*
+*
+*
+*/
+function getName() {
+    playerName = document.getElementsByTagName("name");
+    console.log(playerName);
+    document.getElementsByClassName(".enterNameDiv").style.display = "none";
+}
